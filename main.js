9f842d1be93877d704003d218fd4bc008b4c58f8
DylanFerger <fergdyl@gmail.com>
2025-10-10 14:01:51 -0400

    day 4

--- main.js
+++ main.js
@@ -44,3 +44,16 @@ function spEng(sentence){
 
 
 // Day 4
+
+// There was a test in your class and you passed it. Congratulations!
+
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return true if you're better, else false!
+
+function betterThanAverage(classPoints, yourPoints) {
+  let classPointsSum = classPoints.reduce((a,b) => a + b)/classPoints.length
+  return yourPoints > classPointsSum
+}
