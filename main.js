c1713b878dc4531fd8193f1d39b1c78f5fc52b70
DylanFerger <fergdyl@gmail.com>
2025-10-10 21:27:31 -0400

    day 5

--- main.js
+++ main.js
@@ -57,3 +57,15 @@ function betterThanAverage(classPoints, yourPoints) {
   let classPointsSum = classPoints.reduce((a,b) => a + b)/classPoints.length
   return yourPoints > classPointsSum
 }
+
+// Day 5
+
+// You're re-designing a blog, and the blog's posts have the Weekday Month Day, time format for showing the date and time when a post was made, e.g., Friday May 2, 7pm.
+
+// You're running out of screen real estate, and on some pages you want to display a shorter format, Weekday Month Day that omits the time.
+
+// Write a function that takes the website date/time in its original string format and returns the shortened format.
+
+function shortenToDate(longDate) {
+  return longDate.split(",")[0];
+  }
